dc27ac5448a63ae9f3ffa7dbbeef887d42920a47
Dradoom <dustinhuelsmann@web.de>
2023-09-20 14:22:32 +0200

    Empty side works

---
+++ README.md
@@ -0,0 +1,13 @@
+## Setup env
+DO THIS IN VSCODE
+`python -m venv .venv`
+
+`./.venv/Scripts/activate.ps1`
+
+`pip install -r requirements.txt`
+
+`copy template.env .env`
+
+## Run Solution
+
+`flask run app.py`
---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+from flask_bootstrap import Bootstrap5
+
+load_dotenv() 
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route("/", methods=["POST", "GET"])
+@app.route("/index", methods=["POST", "GET"])
+def index():
+    return render_template("index.html")
